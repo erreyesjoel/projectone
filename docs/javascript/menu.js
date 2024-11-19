3127ac033e1ab3ccf1c52c55bfe3fa16fe74a0e7
erreyesjoel <erreyesjoel.gms@gmail.com>
2024-11-19 16:26:15 +0100

    klk

---
+++ docs/javascript/menu.js
@@ -0,0 +1,5 @@
+/* este js es para el menu, para abrir y cerrarlo */
+const toggleMenu = () => {
+  const menu = document.getElementById("menuDropdown");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+};
